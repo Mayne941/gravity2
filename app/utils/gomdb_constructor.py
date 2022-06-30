872e6b52493a3a39ac1ba093cd0a552f0c9432cc
Richard Mayne <mayne941@gmail.com>
2022-06-30 16:23:31 +0100

    feat: functional py2 pipeline_i

---
+++ app/utils/gomdb_constructor.py
@@ -0,0 +1,6 @@
+def GOMDB_Constructor (TaxoGroupingList, PPHMMLocationTable, GOMIDList):
+	GOMDB = {}
+	for GOMID in GOMIDList:
+		GOMDB[GOMID] = PPHMMLocationTable[TaxoGroupingList == GOMID,:]
+	
+	return GOMDB
---
+++ app/utils/str_to_bool.py
@@ -0,0 +1,5 @@
+def str2bool(v):
+	if isinstance(v, bool):
+		return v
+	elif isinstance(v, str):
+		return v.lower() in ("yes", "true", "t", "1")
