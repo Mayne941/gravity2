4fa86cb555c89c5e5fba428bf278e1dec0a6b464
Richard Mayne <mayne941@gmail.com>
2022-09-02 09:49:46 +0100

    test: pre ch app files

--- app/api.py
+++ app/api.py
@@ -21,10 +21,10 @@ tags_metadata = [
 ]
 
 app = FastAPI(
-    title = "GRAViTy V2",
-    version = "1.0",
-    description = description,
-    contact = {
+    title="GRAViTy V2",
+    version="1.0",
+    description=description,
+    contact={
         "name": "Mayne Bio Analytics Ltd",
         "url": "http://www.mayneba.com",
         "email": "director@mayneba.com"
@@ -34,38 +34,48 @@ app = FastAPI(
         "name": "Apache 2.0",
         "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
     },
-    openapi_tags = tags_metadata
+    openapi_tags=tags_metadata
 )
 
 '''Dev Endpoints'''
+
+
 @app.get("/")
 async def read_root():
     return {"response": "healthy"}
+
 
 @app.get("/refresh_vmr/")
 async def refresh_vmr():
     return "Endpoint under construction"
 
 '''PL1 Entrypoints'''
+
+
 @app.post("/pipeline_i_full/", tags=["Pipeline I"])
 async def pipeline_i_full(payload: Pipeline_i_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
     background_tasks.add_task(run_pipeline_i_full, payload)
     return "Task fired successfully, running in background"
 
+
 def run_pipeline_i_full(payload):
     pl = Pipeline_I(payload)
     pl.read_genome_desc_table()
 
+
 @app.post("/pipeline_i_from_pphmmdb_construction/", tags=["Pipeline I"])
 async def pipeline_i__from_pphmmdb_construction(payload: Pipeline_i_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
-    background_tasks.add_task(run_pipeline_i_from_pphmmdb_construction, payload)
+    background_tasks.add_task(
+        run_pipeline_i_from_pphmmdb_construction, payload)
     return "Task fired successfully, running in background"
+
 
 def run_pipeline_i_from_pphmmdb_construction(payload):
     pl = Pipeline_I(payload)
     pl.pphmmdb_construction()
+
 
 @app.post("/pipeline_i_from_ref_virus_annotator/", tags=["Pipeline I"])
 async def pipeline_i__from_ref_virus_annotator(payload: Pipeline_i_data, background_tasks: BackgroundTasks):
@@ -73,9 +83,11 @@ async def pipeline_i__from_ref_virus_annotator(payload: Pipeline_i_data, backgro
     background_tasks.add_task(run_pipeline_i_from_ref_virus_annotator, payload)
     return "Task fired successfully, running in background"
 
+
 def run_pipeline_i_from_ref_virus_annotator(payload):
     pl = Pipeline_I(payload)
     pl.ref_virus_annotator()
+
 
 @app.post("/pipeline_i_from_graph_generator/", tags=["Pipeline I"])
 async def pipeline_i__from_graph_generator(payload: Pipeline_i_data, background_tasks: BackgroundTasks):
@@ -83,56 +95,73 @@ async def pipeline_i__from_graph_generator(payload: Pipeline_i_data, background_
     background_tasks.add_task(run_pipeline_i_from_graph_generator, payload)
     return "Task fired successfully, running in background"
 
+
 def run_pipeline_i_from_graph_generator(payload):
     pl = Pipeline_I(payload)
     pl.make_graphs()
 
+
 @app.post("/pipeline_i_from_mutual_info_calculator/", tags=["Pipeline I"])
 async def pipeline_i_from_mutual_info_calculator(payload: Pipeline_i_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
-    background_tasks.add_task(run_pipeline_i_from_mutual_info_calculator, payload)
+    background_tasks.add_task(
+        run_pipeline_i_from_mutual_info_calculator, payload)
     return "Task fired successfully, running in background"
+
 
 def run_pipeline_i_from_mutual_info_calculator(payload):
     pl = Pipeline_I(payload)
     pl.mutual_info_calculator()
 
+
 '''PL2 entrypoints'''
+
+
 @app.post("/pipeline_ii_full/", tags=["Pipeline II"])
 async def pipeline_ii_full(payload: Pipeline_ii_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
     background_tasks.add_task(run_pipeline_ii_full, payload)
     return "Task fired successfully, running in background"
 
+
 def run_pipeline_ii_full(payload):
     pl = Pipeline_II(payload)
     pl.read_genome_desc_table()
 
+
 @app.post("/pipeline_ii_from_pphmmdb_construction/", tags=["Pipeline II"])
 async def pipeline_ii_from_pphmmdb_construction(payload: Pipeline_ii_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
-    background_tasks.add_task(run_pipeline_ii_from_pphmmdb_construction, payload)
+    background_tasks.add_task(
+        run_pipeline_ii_from_pphmmdb_construction, payload)
     return "Task fired successfully, running in background"
+
 
 def run_pipeline_ii_from_pphmmdb_construction(payload):
     pl = Pipeline_II(payload)
     pl.pphmmdb_construction()
 
+
 @app.post("/pipeline_ii_from_ucf_virus_annotator/", tags=["Pipeline II"])
 async def pipeline_ii_from_ucf_virus_annotator(payload: Pipeline_ii_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
-    background_tasks.add_task(run_pipeline_ii_from_ucf_virus_annotator, payload)
+    background_tasks.add_task(
+        run_pipeline_ii_from_ucf_virus_annotator, payload)
     return "Task fired successfully, running in background"
+
 
 def run_pipeline_ii_from_ucf_virus_annotator(payload):
     pl = Pipeline_II(payload)
     pl.ucf_virus_annotator()
 
+
 @app.post("/pipeline_ii_from_virus_classification/", tags=["Pipeline II"])
 async def pipeline_ii_from_virus_classification(payload: Pipeline_ii_data, background_tasks: BackgroundTasks):
     payload = jsonable_encoder(payload)
-    background_tasks.add_task(run_pipeline_ii_from_virus_classification, payload)
+    background_tasks.add_task(
+        run_pipeline_ii_from_virus_classification, payload)
     return "Task fired successfully, running in background"
+
 
 def run_pipeline_ii_from_virus_classification(payload):
     pl = Pipeline_II(payload)
--- app/pipeline_ii.py
+++ app/pipeline_ii.py
@@ -7,131 +7,145 @@ from app.utils.str_to_bool import str2bool
 from app.utils.benchmark import benchmark_start, benchmark_end
 from app.utils.generate_logs import Log_Generator_Pl2
 
-import optparse, os, time
+import optparse
+import os
+import time
+
 
 class Pipeline_II:
-	def __init__(self, payload):
-		self.options = payload
-		'''Create directories'''
-		if not os.path.exists(self.options["ShelveDir_UcfVirus"]):
-			os.makedirs(self.options["ShelveDir_UcfVirus"])
-		'''Logs'''
-		self.log_gen = Log_Generator_Pl2(self.options, self.options["ShelveDir_UcfVirus"])
-		self.actual_start = time.time()
-		self.logs = self.log_gen.entrypoint()
-		'''Catch bad database flags'''
-		if (self.options['Database'] != None and self.options['Database_Header'] == None):
-			raise optparse.OptionValueError(f"You have specified DATABASE as {self.options['Database']}, 'Database_Header' cannot be 'None'")
-		if (self.options['Database'] == None and self.options['Database_Header'] != None):
-			Proceed = input (f"You have specified 'Database_Header' as {self.options['Database_Header']}, but 'Database' is 'None'. GRAViTy will analyse all genomes. Do you want to proceed? [Y/n]: ")
-			if Proceed != "Y":
-				raise SystemExit("GRAViTy terminated.")
-		
-	def read_genome_desc_table(self):
-		'''I: Fire Read Genom Desc Table'''
-		[print(log_text) for log_text in self.logs[0]]
-		start = benchmark_start("ReadGenomeDescTable")
-		rgdt = ReadGenomeDescTable(
-			GenomeDescTableFile	= self.options['GenomeDescTableFile_UcfVirus'],
-			ShelveDir			= self.options['ShelveDir_UcfVirus'],
-			Database			= self.options['Database'],
-			Database_Header		= self.options['Database_Header'],
-			)
-		rgdt.entrypoint()
-		benchmark_end("ReadGenomeDescTable", start)
-		
-		self.pphmmdb_construction()
+    def __init__(self, payload):
+        self.options = payload
+        '''Create directories'''
+        if not os.path.exists(self.options["ShelveDir_UcfVirus"]):
+            os.makedirs(self.options["ShelveDir_UcfVirus"])
+        '''Logs'''
+        self.log_gen = Log_Generator_Pl2(
+            self.options, self.options["ShelveDir_UcfVirus"])
+        self.actual_start = time.time()
+        self.logs = self.log_gen.entrypoint()
+        '''Catch bad database flags'''
+        if (self.options['Database'] != None and self.options['Database_Header'] == None):
+            raise optparse.OptionValueError(
+                f"You have specified DATABASE as {self.options['Database']}, 'Database_Header' cannot be 'None'")
+        if (self.options['Database'] == None and self.options['Database_Header'] != None):
+            Proceed = input(
+                f"You have specified 'Database_Header' as {self.options['Database_Header']}, but 'Database' is 'None'. GRAViTy will analyse all genomes. Do you want to proceed? [Y/n]: ")
+            if Proceed != "Y":
+                raise SystemExit("GRAViTy terminated.")
 
-	def pphmmdb_construction(self):
-		'''II: Fire PPHMDB Constructor'''
-		if str2bool(self.options['UseUcfVirusPPHMMs']) == True:
-			[print(log_text) for log_text in self.logs[1]]				
-			start = benchmark_start("PPHMMDBConstruction")
-			pc = PPHMMDBConstruction (
-				GenomeSeqFile = self.options['GenomeSeqFile_UcfVirus'],
-				ShelveDir = self.options['ShelveDir_UcfVirus'],
-				ProteinLength_Cutoff = self.options['ProteinLength_Cutoff'],
-				IncludeIncompleteGenomes = str2bool(self.options['IncludeProteinsFromIncompleteGenomes']),
-				BLASTp_evalue_Cutoff = self.options['BLASTp_evalue_Cutoff'],
-				BLASTp_PercentageIden_Cutoff = self.options['BLASTp_PercentageIden_Cutoff'],
-				BLASTp_QueryCoverage_Cutoff = self.options['BLASTp_QueryCoverage_Cutoff'],
-				BLASTp_SubjectCoverage_Cutoff = self.options['BLASTp_SubjectCoverage_Cutoff'],
-				BLASTp_num_alignments = self.options['BLASTp_num_alignments'],
-				BLASTp_N_CPUs = self.options['BLASTp_N_CPUs'],
-				MUSCLE_GapOpenCost = self.options['MUSCLE_GapOpenCost'],
-				MUSCLE_GapExtendCost = self.options['MUSCLE_GapExtendCost'],
-				ProtClustering_MCLInflation = self.options['ProtClustering_MCLInflation'],
-				N_AlignmentMerging = self.options['N_AlignmentMerging'],
-				HHsuite_evalue_Cutoff = self.options['HHsuite_evalue_Cutoff'],
-				HHsuite_pvalue_Cutoff = self.options['HHsuite_pvalue_Cutoff'],
-				HHsuite_N_CPUs = self.options['HHsuite_N_CPUs'],
-				HHsuite_QueryCoverage_Cutoff = self.options['HHsuite_QueryCoverage_Cutoff'],
-				HHsuite_SubjectCoverage_Cutoff = self.options['HHsuite_SubjectCoverage_Cutoff'],
-				PPHMMClustering_MCLInflation = self.options['PPHMMClustering_MCLInflation_ForAlnMerging'],
-				HMMER_PPHMMDb_ForEachRoundOfPPHMMMerging = str2bool(self.options['HMMER_PPHMMDB_ForEachRoundOfPPHMMMerging']),
-				)
-			pc.main()
-			benchmark_end("PPHMMDBConstruction", start)
+    def read_genome_desc_table(self):
+        '''I: Fire Read Genom Desc Table'''
+        [print(log_text) for log_text in self.logs[0]]
+        start = benchmark_start("ReadGenomeDescTable")
+        rgdt = ReadGenomeDescTable(
+            GenomeDescTableFile=self.options['GenomeDescTableFile_UcfVirus'],
+            ShelveDir=self.options['ShelveDir_UcfVirus'],
+            Database=self.options['Database'],
+            Database_Header=self.options['Database_Header'],
+        )
+        rgdt.entrypoint()
+        benchmark_end("ReadGenomeDescTable", start)
 
-		self.ucf_virus_annotator()
+        self.pphmmdb_construction()
 
-	def ucf_virus_annotator(self):
-		'''III: Fire UCF Virus Annotator'''
-		[print(log_text) for log_text in self.logs[2]]
-		start = benchmark_start("UcfVirusAnnotator")
-		ucf = UcfVirusAnnotator (
-			GenomeSeqFile_UcfVirus = self.options['GenomeSeqFile_UcfVirus'],
-			ShelveDir_UcfVirus = self.options['ShelveDir_UcfVirus'],
-			ShelveDirs_RefVirus = self.options['ShelveDirs_RefVirus'],
-			IncludeIncompleteGenomes_UcfVirus = str2bool(self.options['AnnotateIncompleteGenomes_UcfVirus']),
-			IncludeIncompleteGenomes_RefVirus = str2bool(self.options['UsingDatabaseIncludingIncompleteRefViruses']),
-			SeqLength_Cutoff = 0,
-			HMMER_N_CPUs = self.options['HMMER_N_CPUs'],
-			HMMER_C_EValue_Cutoff = self.options['HMMER_C_EValue_Cutoff'],
-			HMMER_HitScore_Cutoff = self.options['HMMER_HitScore_Cutoff'],
-			)
-		ucf.main()
-		benchmark_end("UcfVirusAnnotator", start)
+    def pphmmdb_construction(self):
+        '''II: Fire PPHMDB Constructor'''
+        if str2bool(self.options['UseUcfVirusPPHMMs']) == True:
+            [print(log_text) for log_text in self.logs[1]]
+            start = benchmark_start("PPHMMDBConstruction")
+            pc = PPHMMDBConstruction(
+                GenomeSeqFile=self.options['GenomeSeqFile_UcfVirus'],
+                ShelveDir=self.options['ShelveDir_UcfVirus'],
+                ProteinLength_Cutoff=self.options['ProteinLength_Cutoff'],
+                IncludeIncompleteGenomes=str2bool(
+                    self.options['IncludeProteinsFromIncompleteGenomes']),
+                BLASTp_evalue_Cutoff=self.options['BLASTp_evalue_Cutoff'],
+                BLASTp_PercentageIden_Cutoff=self.options['BLASTp_PercentageIden_Cutoff'],
+                BLASTp_QueryCoverage_Cutoff=self.options['BLASTp_QueryCoverage_Cutoff'],
+                BLASTp_SubjectCoverage_Cutoff=self.options['BLASTp_SubjectCoverage_Cutoff'],
+                BLASTp_num_alignments=self.options['BLASTp_num_alignments'],
+                BLASTp_N_CPUs=self.options['BLASTp_N_CPUs'],
+                MUSCLE_GapOpenCost=self.options['MUSCLE_GapOpenCost'],
+                MUSCLE_GapExtendCost=self.options['MUSCLE_GapExtendCost'],
+                ProtClustering_MCLInflation=self.options['ProtClustering_MCLInflation'],
+                N_AlignmentMerging=self.options['N_AlignmentMerging'],
+                HHsuite_evalue_Cutoff=self.options['HHsuite_evalue_Cutoff'],
+                HHsuite_pvalue_Cutoff=self.options['HHsuite_pvalue_Cutoff'],
+                HHsuite_N_CPUs=self.options['HHsuite_N_CPUs'],
+                HHsuite_QueryCoverage_Cutoff=self.options['HHsuite_QueryCoverage_Cutoff'],
+                HHsuite_SubjectCoverage_Cutoff=self.options['HHsuite_SubjectCoverage_Cutoff'],
+                PPHMMClustering_MCLInflation=self.options['PPHMMClustering_MCLInflation_ForAlnMerging'],
+                HMMER_PPHMMDb_ForEachRoundOfPPHMMMerging=str2bool(
+                    self.options['HMMER_PPHMMDB_ForEachRoundOfPPHMMMerging']),
+            )
+            pc.main()
+            benchmark_end("PPHMMDBConstruction", start)
 
-		self.virus_classification()
+        self.ucf_virus_annotator()
 
-	def virus_classification(self):
-		'''IV: Fire Classifiers'''
-		[print(log_text) for log_text in self.logs[3]]
-		start = benchmark_start("VirusClassificationAndEvaluation")
-		vce = VirusClassificationAndEvaluation (
-			ShelveDir_UcfVirus = self.options['ShelveDir_UcfVirus'],
-			ShelveDirs_RefVirus = self.options['ShelveDirs_RefVirus'],
-			IncludeIncompleteGenomes_UcfVirus = str2bool(self.options['AnnotateIncompleteGenomes_UcfVirus']),
-			IncludeIncompleteGenomes_RefVirus = str2bool(self.options['UsingDatabaseIncludingIncompleteRefViruses']),
-			UseUcfVirusPPHMMs = str2bool(self.options['UseUcfVirusPPHMMs']),
-			GenomeSeqFile_UcfVirus = self.options['GenomeSeqFile_UcfVirus'],
-			GenomeSeqFiles_RefVirus = self.options['GenomeSeqFiles_RefVirus'],
-			SeqLength_Cutoff = 0,
-			HMMER_N_CPUs = int(self.options['HMMER_N_CPUs']),
-			HMMER_C_EValue_Cutoff = float(self.options['HMMER_C_EValue_Cutoff']),
-			HMMER_HitScore_Cutoff = float(self.options['HMMER_HitScore_Cutoff']),
-			SimilarityMeasurementScheme = self.options['SimilarityMeasurementScheme'],
-			p = float(self.options['p']),
-			Dendrogram_LinkageMethod = self.options['Dendrogram_LinkageMethod'],
-			DatabaseAssignmentSimilarityScore_Cutoff = float(self.options['DatabaseAssignmentSimilarityScore_Cutoff']),
-			N_PairwiseSimilarityScores = int(self.options['N_PairwiseSimilarityScores']),
-			Heatmap_WithDendrogram = str2bool(self.options['Heatmap_WithDendrogram']),
-			Heatmap_DendrogramSupport_Cutoff = float(self.options['Heatmap_DendrogramSupport_Cutoff']),
-			Bootstrap = str2bool(self.options['Bootstrap']),
-			N_Bootstrap = int(self.options['N_Bootstrap']),
-			Bootstrap_method = self.options['Bootstrap_method'],
-			Bootstrap_N_CPUs = int(self.options['Bootstrap_N_CPUs']),
-			VirusGrouping = str2bool(self.options['VirusGrouping']),
-			)
-		vce.main()
-		benchmark_end("VirusClassificationAndEvaluation", start)	
+    def ucf_virus_annotator(self):
+        '''III: Fire UCF Virus Annotator'''
+        [print(log_text) for log_text in self.logs[2]]
+        start = benchmark_start("UcfVirusAnnotator")
+        ucf = UcfVirusAnnotator(
+            GenomeSeqFile_UcfVirus=self.options['GenomeSeqFile_UcfVirus'],
+            ShelveDir_UcfVirus=self.options['ShelveDir_UcfVirus'],
+            ShelveDirs_RefVirus=self.options['ShelveDirs_RefVirus'],
+            IncludeIncompleteGenomes_UcfVirus=str2bool(
+                self.options['AnnotateIncompleteGenomes_UcfVirus']),
+            IncludeIncompleteGenomes_RefVirus=str2bool(
+                self.options['UsingDatabaseIncludingIncompleteRefViruses']),
+            SeqLength_Cutoff=0,
+            HMMER_N_CPUs=self.options['HMMER_N_CPUs'],
+            HMMER_C_EValue_Cutoff=self.options['HMMER_C_EValue_Cutoff'],
+            HMMER_HitScore_Cutoff=self.options['HMMER_HitScore_Cutoff'],
+        )
+        ucf.main()
+        benchmark_end("UcfVirusAnnotator", start)
 
-		print(f"Time to complete: {time.time() - self.actual_start}")
+        self.virus_classification()
+
+    def virus_classification(self):
+        '''IV: Fire Classifiers'''
+        [print(log_text) for log_text in self.logs[3]]
+        start = benchmark_start("VirusClassificationAndEvaluation")
+        vce = VirusClassificationAndEvaluation(
+            ShelveDir_UcfVirus=self.options['ShelveDir_UcfVirus'],
+            ShelveDirs_RefVirus=self.options['ShelveDirs_RefVirus'],
+            IncludeIncompleteGenomes_UcfVirus=str2bool(
+                self.options['AnnotateIncompleteGenomes_UcfVirus']),
+            IncludeIncompleteGenomes_RefVirus=str2bool(
+                self.options['UsingDatabaseIncludingIncompleteRefViruses']),
+            UseUcfVirusPPHMMs=str2bool(self.options['UseUcfVirusPPHMMs']),
+            GenomeSeqFile_UcfVirus=self.options['GenomeSeqFile_UcfVirus'],
+            GenomeSeqFiles_RefVirus=self.options['GenomeSeqFiles_RefVirus'],
+            SeqLength_Cutoff=0,
+            HMMER_N_CPUs=int(self.options['HMMER_N_CPUs']),
+            HMMER_C_EValue_Cutoff=float(self.options['HMMER_C_EValue_Cutoff']),
+            HMMER_HitScore_Cutoff=float(self.options['HMMER_HitScore_Cutoff']),
+            SimilarityMeasurementScheme=self.options['SimilarityMeasurementScheme'],
+            p=float(self.options['p']),
+            Dendrogram_LinkageMethod=self.options['Dendrogram_LinkageMethod'],
+            DatabaseAssignmentSimilarityScore_Cutoff=float(
+                self.options['DatabaseAssignmentSimilarityScore_Cutoff']),
+            N_PairwiseSimilarityScores=int(
+                self.options['N_PairwiseSimilarityScores']),
+            Heatmap_WithDendrogram=str2bool(
+                self.options['Heatmap_WithDendrogram']),
+            Heatmap_DendrogramSupport_Cutoff=float(
+                self.options['Heatmap_DendrogramSupport_Cutoff']),
+            Bootstrap=str2bool(self.options['Bootstrap']),
+            N_Bootstrap=int(self.options['N_Bootstrap']),
+            Bootstrap_method=self.options['Bootstrap_method'],
+            Bootstrap_N_CPUs=int(self.options['Bootstrap_N_CPUs']),
+            VirusGrouping=str2bool(self.options['VirusGrouping']),
+        )
+        vce.main()
+        benchmark_end("VirusClassificationAndEvaluation", start)
+
+        print(f"Time to complete: {time.time() - self.actual_start}")
+
 
 if __name__ == '__main__':
-	pl = Pipeline_II()
-	pl.main()
-
-
-
+    pl = Pipeline_II()
+    pl.main()
