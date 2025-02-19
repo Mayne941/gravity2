6a87d0bec52e12f2074e847376a5e8f9876dedfe
Rich Mayne <mayne941@gmail.com>
2025-02-19 17:33:39 +0000

    chore: reqs/readme update

--- app/utils/hhr_parse.py
+++ app/utils/hhr_parse.py
@@ -12,11 +12,6 @@ def hhparse(hhresult_file):
 	pattern = StringIO(get_template()).readlines()[1]
 	colBreaks = [i for i, ch in enumerate(pattern) if ch == '|']
 	widths = [j-i for i, j in zip(([0]+colBreaks)[:-1], colBreaks)]
-	# df = pd.read_fwf(hhresult_file, skiprows=8,
-	#                       nrows=10, header=0, widths=widths)
 	df = pd.read_fwf(hhresult_file, colspecs=(3, 30, 5, 7), nrows=10, skiprows=8)
 	print(df)
 	return df
-
-if __name__ == '__main__':
-	hhparse("test.fuck")
